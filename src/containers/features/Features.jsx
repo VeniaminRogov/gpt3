849dfe9f6003933b25efac0b0101dbc805b9efe9
Veniamin <rogov.veniamin@gmail.com>
2023-01-20 15:21:13 +0700

    add containers

---
+++ src/containers/features/Features.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './features.scss';
+
+function Features() {
+    return <div>Features</div>;
+}
+
+export default Features;
---
+++ src/containers/possibility/Possibility.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './possibility.scss';
+
+function Possibility() {
+    return <div>Possibility</div>;
+}
+
+export default Possibility;
